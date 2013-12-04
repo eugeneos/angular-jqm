1e3a00d84294fd59d890611dcdbdfe4433ee7568
eugeneos <eugene.osovetsky@gmail.com>
2013-12-04 15:09:21 -0500

    Adding repro for the iOS click bug

---
+++ bugRepros/iosClick/bugRepro.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+var bugRepro = angular.module('bugRepro', ['jqm']);
+
+bugRepro.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl: "pageTemplate.html",
+        controller: "PageCtrl"
+    });
+
+    $routeProvider.otherwise({
+        redirectTo: '/'
+    });
+
+}]);
+
+bugRepro.controller('PageCtrl', ['$scope', function ($scope) {
+
+    var i;
+    $scope.items = [];
+    for (i = 1; i <= 100; i++) { $scope.items.push(i); }
+
+    $scope.doClick = function (item) {
+        alert(item);
+    }
+
+}]);
